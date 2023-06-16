52c049d67d3ece67113e2b4f34c5679607c88567
tamy-k <kolawoletamilore1@gmail.com>
2023-06-16 10:14:10 +0100

    feat: build course material listing page

--- lib/screens/course_material_listing.dart
+++ lib/screens/course_material_listing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_study/utils/theme.dart';
 
 class CourseMaterialListing extends StatelessWidget {
   const CourseMaterialListing({super.key});
@@ -9,14 +10,120 @@ class CourseMaterialListing extends StatelessWidget {
       body: Column(
         children: [
           Container(
+            padding: EdgeInsets.symmetric(
+                vertical: MediaQuery.of(context).padding.top, horizontal: 24),
             width: double.maxFinite,
-            height: MediaQuery.of(context).size.height * 0.35,
+            height: 250,
             decoration: const BoxDecoration(
-              color: Color(0xff383838),
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40),bottomRight: Radius.circular(40))
+                color: Color(0xff383838),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40)),
+                image: DecorationImage(
+                    alignment: Alignment.centerRight,
+                    image: AssetImage('assets/back.png'),
+                    fit: BoxFit.fitHeight)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Align(
+                          alignment: AlignmentDirectional.bottomCenter,
+                          child: Text(
+                            "CSC 401",
+                            style: primaryTextStyle.copyWith(fontSize: 32,color: Colors.white,fontWeight: FontWeight.w600),
+                          )),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: GestureDetector(
+                    onTap: (){
+                      print("object");
+                    },
+                    child: const Icon(
+                      Icons.arrow_back_ios_rounded,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
             ),
-            child: Column(),
-          )
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search_rounded),
+                hintText: "Search Course",
+                filled: true,
+                fillColor: const Color.fromARGB(255, 233, 233, 233),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                  ),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                  ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                  ),
+                ),
+                contentPadding: const EdgeInsets.symmetric(vertical: 10,horizontal: 16)
+                
+              ),
+              
+              style: primaryTextStyle,
+            ),
+          ),
+          // const SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 5, itemBuilder: (context, index) => Column(
+                    children: [
+                      ListTile(
+                        leading: const CircleAvatar(
+                          radius: 25,
+                          backgroundColor: Color.fromARGB(255, 228, 228, 228),
+                          // child: Icon(Icons.docu),
+                        ),
+                        title: Text("The intro to lorem ipsum",style:primaryTextStyle),
+                        subtitle: Text('Jun 12',style:primaryTextStyle.copyWith(fontSize: 14,color: Colors.grey)),
+                        trailing: IconButton(
+                          onPressed: (){},
+                          icon: const Icon(Icons.more_vert_rounded),
+                        ),
+                      ),
+                      const Divider(
+                        height: 1,
+                        thickness: 4,
+                      )
+                    ],
+                  )))
         ],
       ),
     );
